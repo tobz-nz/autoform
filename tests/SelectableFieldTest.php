94a2337930da2345af93b8c70664a41a308adefc
Toby Evans <tobz.nz@gmail.com>
2015-01-19 22:10:20 +1300

    finish SelectableField tests

--- tests/SelectableFieldTest.php
+++ tests/SelectableFieldTest.php
@@ -26,28 +26,50 @@ class SelectableFieldTest extends \PHPUnit_Framework_TestCase
 
     public function testHasEmptyValue()
     {
-        $field = new SelectField('test', [], 0);
+        $field = new SelectField('test', ['--no value--', 'a value', 'another value'], 0);
         $instance = $field->hasEmptyValue(true);
         $this->assertInstanceOf('Tobz\Autoform\Fields\SelectField', $instance);
-        // $this->assertEquals('', $field->getValue());
+        $this->assertEquals(0, $field->getValue());
+        $this->assertEquals(
+            '<select id="test" name="test">
+<option selected>--no value--</option>
+<option value="1">a value</option>
+<option value="2">another value</option>
+</select>',
+            (string) $field
+        );
 
     }
 
     public function testIsSelected()
     {
-        $field = new SelectField('test', []);
+        $field = new SelectField('test', ['test 1', 'test 2', 'test 3'], 1);
+        $this->assertFalse($field->isSelected(0));
+        $this->assertFalse($field->isSelected(2));
+        $this->assertTrue($field->isSelected(1));
+
+        $field->setValue(2);
+        $this->assertFalse($field->isSelected(1));
+        $this->assertFalse($field->isSelected(3));
+        $this->assertTrue($field->isSelected(2));
 
     }
 
     public function testGetOptions()
     {
-        $field = new SelectField('test', []);
+        $field = new SelectField('test', ['test 1', 'test 2', 'test 3'], 1);
+        $this->assertEquals(['test 1', 'test 2', 'test 3'], $field->getOptions());
 
     }
 
     public function testRenderOptions()
     {
-        $field = new SelectField('test', []);
-
+        $field = new SelectField('test', ['test 1', 'test 2', 'test 3'], 1);
+        $this->assertEquals(
+            '<option value="0">test 1</option>
+<option value="1" selected>test 2</option>
+<option value="2">test 3</option>',
+            $field->renderOptions()
+        );
     }
 }
